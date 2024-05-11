d43fbbf47897619cf9e9a902ce10fc07a5f9bdef
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-05-11 00:49:13 -0300

    printing title

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-console.log("code Here")
-export function sum(a:number,b:number){
-    return a+b
+import { printAsciiArt } from "./utils/printTitle";
+import chalk from "chalk"
+import path from "node:path"
+import fs from "fs"
+import inquirer from "inquirer";
+async function main(){
+    await printAsciiArt("TermiWallet!")
+    console.log(chalk.italic.bold("Your favorite Ethereum CLI!"))
+
+    if(fs.existsSync(path.resolve(__dirname,"..","wallet-data","wallet.json"))){
+        
+    }
+
+    
+    
+    
 }
+
+
+main()
+.catch((err)=>{
+    console.error(err)
+    process.exit(1)
+})
