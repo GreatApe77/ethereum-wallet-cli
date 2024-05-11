27358e191c12a4365234c4b96700ff2b4f673ee0
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-05-11 01:58:38 -0300

    show generated Wallet on screen

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
-import { printAsciiArt } from "./utils/printTitle";
-import chalk from "chalk"
-import path from "node:path"
-import fs from "fs"
+import { printAsciiArt } from "./utils/logs/printTitle";
+import chalk from "chalk";
+import path from "node:path";
+import fs from "fs";
 import inquirer from "inquirer";
-async function main(){
-    await printAsciiArt("TermiWallet!")
-    console.log(chalk.italic.bold("Your favorite Ethereum CLI!"))
+import { printNewWalletMenu } from "./utils/logs/printNewWalletMenu";
+import { ethers } from "ethers";
+import {
+  ImportOrCreateChoices,
+  promptImportOrCreate,
+} from "./utils/prompts/promptImportOrCreateWallet";
+import { printMnemonic } from "./utils/logs/printMnemonic";
+import { printLineSpace } from "./utils/logs/printLineSpace";
+import { printSaveMnemonicAlert } from "./utils/logs/printSaveMnemonicAlert";
+import { printExplainMnemonicPhrase } from "./utils/logs/printExplainMnemonicPhrase";
+let wallet: ethers.HDNodeWallet | ethers.Wallet;
+async function main() {
+  await printAsciiArt("TermiWallet!");
+  console.log(chalk.italic.bold("Your favorite Ethereum CLI!"));
 
-    if(fs.existsSync(path.resolve(__dirname,"..","wallet-data","wallet.json"))){
-        
+  if (fs.existsSync(path.resolve(__dirname, "..", "wallet-data", "wallet.json"))) {
+    printNewWalletMenu();
+    const choice = await promptImportOrCreate();
+    if (choice == ImportOrCreateChoices.CREATE_NEW_WALLET) {
+      wallet = ethers.Wallet.createRandom();
+      printLineSpace()
+      printExplainMnemonicPhrase()
+      printMnemonic(wallet.mnemonic?.phrase!)
+      printSaveMnemonicAlert()
+    } else if (choice == ImportOrCreateChoices.IMPORT) {
     }
-
-    
-    
-    
+  }
 }
 
-
-main()
-.catch((err)=>{
-    console.error(err)
-    process.exit(1)
-})
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
