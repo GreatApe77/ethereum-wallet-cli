cc037b574a3906331171b7b13e7cfc100379f45f
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-05-24 16:40:24 -0300

    add routine todos

---
+++ src/routines/switchChainRoutine.ts
@@ -0,0 +1,8 @@
+
+
+export async function swithChainsRoutine(){
+    //print switch chain menu that should contain current chain information
+    //option to return to main menu
+    //list of available chains
+    //switch chain and save information to the user state file
+}
