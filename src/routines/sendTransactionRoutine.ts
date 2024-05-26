8f5078bb66a02ed13e770421ebfed9a40157cfc1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-05-26 10:50:24 -0300

    add todos for send Transaction task

---
+++ src/routines/sendTransactionRoutine.ts
@@ -0,0 +1,8 @@
+export async function sendTransactionRoutine(){
+    //print transaction menu with the current account chain name and balance
+    //prompt:
+    // 1. toAddress 
+    // 2. value (in ether)
+    // 3. request confirmation showing the transaction data
+    // 4 execute the transaction
+}
