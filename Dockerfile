174078ae814b139927c5498b8419a86c6156a359
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-05-26 22:06:12 -0300

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:20-alpine3.17
+
+WORKDIR /app
+
+COPY package.json .
+
+COPY package-lock.json .
+
+RUN npm ci
+
+RUN npm run build
+
+CMD [ "npm","run","start" ]
--- src/utils/loadUserOptionsState.ts
+++
@@ -1,9 +0,0 @@
-import fs from "node:fs/promises"
-import { userOptionsFilePath } from "../constants/paths.js"
-import { UserOptionsState } from "../lib/UserOptionsState.js"
-
-
-export async function loadUserOptionsState(){
-    const userOptionsState = await fs.readFile(userOptionsFilePath,"utf-8")
-    return new UserOptionsState(JSON.parse(userOptionsState))
-}
