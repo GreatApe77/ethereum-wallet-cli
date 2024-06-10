231270ac95121c332582865cd2f0deb81b7f8b7a
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-06-10 17:01:06 -0300

    load tokens in menu

--- src/routines/erc20TokensMenuRoutine.ts
+++ src/routines/erc20TokensMenuRoutine.ts
@@ -1,6 +1,15 @@
 import { menuTitle } from "../components/menuTitle.js";
+import { getUserOptionsState } from "../constants/userOptionsStateSingleton.js";
+import { getErc20TokenRepository } from "../repositories/implementations/erc20-token-repository/singleton.js";
 import { printMarginLeft } from "../utils/printMarginLeft.js";
 
 export async function erc20TokensMenuRoutine(){
     menuTitle("TOKENS (ERC20)")
+    let config = getUserOptionsState()
+    let tokensRepository = getErc20TokenRepository()
+    
+    let tokens = (await tokensRepository.getAll()).filter(token=>token.chainId===config.chainId)
+
+    
+
 }
